0e55a45a5d718b396f4928cbb588113417949f1e
Zel Start <134749644+zelstart@users.noreply.github.com>
2024-01-08 19:27:31 -0500

    fixed import path

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx'
 
-import Home from './views/Home/Home.jsx'
+import Home from './pages/Home/Home.jsx'
 
 const router = createBrowserRouter([
   {
